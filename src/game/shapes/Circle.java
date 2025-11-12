360c462b2fc093c15dd0a76284e227e345490afd
Therunner05 <89034241+Therunner05@users.noreply.github.com>
2025-11-12 15:39:51 -0500

    finished my rendition of refactoring the shapes classes.

--- src/game/shapes/Circle.java
+++ src/game/shapes/Circle.java
@@ -10,9 +10,8 @@ public class Circle extends Shape {
     private double x;
     private double y;
     private double r = 0;
-    private String oval = "not oval"; //this also gets reused for the text making thing.
-    private double w;
-    private double h;
+    private double w = 0;
+    private double h = 0;
 
     public Circle(double x, double y, double radius, Color color) {
         super();
@@ -26,13 +25,12 @@ public class Circle extends Shape {
         this.x = x;
         this.y = y;
         this.color = color;
-        this.oval = "oval";
         this.h = height;
         this.w = width;
     }
 
     public void draw(GraphicsContext gc) {
-        if (oval.equals("oval")) {
+        if (this.r==0) {
             gc.setFill(color);
             gc.fillOval(x, y, w, h);
         } else {
